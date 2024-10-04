9bf1818bb9c4973d8a835521b04769fa70f9536f
iamravinder1629 <iamravinder1629@gmail.com>
2024-10-04 19:07:00 +0530

    node server is created

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const {add, sub}= require("./my-math")
+const http = require('http');
+const fs = require('fs');
 
-console.log(add(10, 10))
-console.log(sub(1, 10))
+const index = fs.readFileSync("./index.html", "utf8");
+const jsonData = fs.readFileSync("./data.json", "utf8");
+
+const server = http.createServer((req, res) => {
+    switch (req.url) {
+        case "/":
+            res.setHeader("content-type", "text/html");
+            res.end(index);
+            break;
+        case "/users":
+            res.setHeader("content-type", "application/json")
+            res.end(jsonData)
+            break;
+        default:
+            res.writeHeader(404)
+            res.end("page not found");
+    }
+
+})
+
+server.listen(8000, () => { console.log("server listening on port 8000") })
